85e6798f63bfe436b6b6aacfa8cdc5eb55117786
Doug Masten <doug@dougmasten.com>
2019-03-23 02:07:04 -0500

    commit macros file

---
+++ common/macros.asm
@@ -0,0 +1,71 @@
+; macros.asm
+                    *pragmapush list        ; Save state of list pragma
+                    pragma nolist           ; Turn off assembly listing and exclude from symbol list
+
+
+; space savings macros
+skip_one_byte       MACRO
+                    fcb $c5                 ; actually a BITB #$xx instruction (2 clocks)
+                    ENDM
+
+skip_two_bytes      MACRO
+                    fcb $8c                 ; actually a CMPX #$xxxx instruction (3 clocks)
+                    ENDM
+
+
+; convenient macros
+disable_interrupts  MACRO
+                    orcc #$50
+                    ENDM
+
+enable_interrupts   MACRO
+                    andcc #$af
+                    ENDM
+
+clru                MACRO
+                    ldu #0
+                    ENDM
+
+clrx                MACRO
+                    ldx #0
+                    ENDM
+
+clry                MACRO
+                    ifpragma 6809
+                      ldy #0
+                    else
+                      tfr 0,y
+                    endc
+                    ENDM
+
+clrq                MACRO
+                    clrd
+                    clrw
+                    ENDM
+
+tstu                MACRO
+                    ifpragma 6809
+                      cmpu #0
+                    else
+                      cmpr u,0
+                    endc
+                    ENDM
+
+tstx                MACRO
+                    cmpx #0
+                    ENDM
+
+tsty                MACRO
+                    ifpragma 6809
+                      cmpy #0
+                    else
+                      cmpr y,0
+                    endc
+                    ENDM
+
+tstq                MACRO
+                    stq -4,s
+                    ENDM
+
+
+                    *pragmapop list         ; restore assembly listing to previous state
