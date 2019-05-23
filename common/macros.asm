1a23a10680eb845718208e01a79cee0b777190f8
Doug Masten <doug@dougmasten.com>
2019-05-23 03:55:40 -0500

    sort CC register flags convenient macros and add CC flag definitions

--- common/macros.asm
+++ common/macros.asm
@@ -4,6 +4,17 @@
                     ifndef MACROS_DEFS      ; Load macros only once
 
 
+; convenient definitions
+CC_STACK            equ %10000000           ; entire register state stacked
+CC_FIRQ             equ %01000000           ; FIRQ interrupt masked
+CC_HCARRY           equ %00100000           ; half-carry
+CC_IRQ              equ %00010000           ; IRQ interrupt masked
+CC_NEGATIVE         equ %00001000           ; negative result (twos complement)
+CC_ZERO             equ %00000100           ; zero result
+CC_OVERFLOW         equ %00000010           ; overflow
+CC_CARRY            equ %00000001           ; carry (or borrow)
+
+
 ; space savings macros
 skip_one_byte       macro
                     fcb $c5                 ; actually a BITB #$xx instruction (2 clocks)
@@ -16,75 +27,75 @@ skip_two_bytes      macro
 
 ; convenient macros
 disable_interrupts  macro
-                    orcc #$50
+                    orcc #CC_FIRQ+CC_IRQ
                     endm
 
 enable_interrupts   macro
-                    andcc #$af
-                    endm
-
-clear_carry_flag    macro
-                    andcc #^1
-                    endm
-
-set_carry_flag      macro
-                    orcc #1
-                    endm
-
-clear_overflow_flag macro
-                    andcc #^%00000010
-                    endm
-
-set_overflow_flag   macro
-                    orcc #%00000010
-                    endm
-
-clear_zero_flag     macro
-                    andcc #^%00000100
-                    endm
-
-set_zero_flag       macro
-                    orcc #%00000100
-                    endm
-
-clear_negative_flag macro
-                    andcc #^%00001000
-                    endm
-
-set_negative_flag   macro
-                    orcc #%00001000
-                    endm
-
-clear_irq_flag      macro
-                    andcc #^%00010000
-                    endm
-
-set_irq_flag        macro
-                    orcc #%00010000
-                    endm
-
-clear_half_carry_flag macro
-                    andcc #^%00100000
-                    endm
-
-set_half_carry_flag macro
-                    orcc #%00100000
-                    endm
-
-clear_firq_flag     macro
-                    andcc #^%01000000
-                    endm
-
-set_firq_flag       macro
-                    orcc #%01000000
+                    andcc #^(CC_FIRQ+CC_IRQ)
                     endm
 
 clear_stack_flag    macro
-                    andcc #^%10000000
+                    andcc #^CC_STACK
                     endm
 
 set_stack_flag      macro
-                    orcc #%10000000
+                    orcc #CC_STACK
+                    endm
+
+clear_firq_flag     macro
+                    andcc #^CC_FIRQ
+                    endm
+
+set_firq_flag       macro
+                    orcc #CC_FIRQ
+                    endm
+
+clear_half_carry_flag macro
+                    andcc #^CC_HCARRY
+                    endm
+
+set_half_carry_flag macro
+                    orcc #CC_HCARRY
+                    endm
+
+clear_irq_flag      macro
+                    andcc #^CC_IRQ
+                    endm
+
+set_irq_flag        macro
+                    orcc #CC_IRQ
+                    endm
+
+clear_negative_flag macro
+                    andcc #^CC_NEGATIVE
+                    endm
+
+set_negative_flag   macro
+                    orcc #CC_NEGATIVE
+                    endm
+
+clear_zero_flag     macro
+                    andcc #^CC_ZERO
+                    endm
+
+set_zero_flag       macro
+                    orcc #CC_ZERO
+                    endm
+
+clear_overflow_flag macro
+                    andcc #^CC_OVERFLOW
+                    endm
+
+set_overflow_flag   macro
+                    orcc #CC_OVERFLOW
+                    endm
+
+clear_carry_flag    macro
+                    andcc #^CC_CARRY
+                    endm
+
+set_carry_flag      macro
+                    orcc #CC_CARRY
                     endm
 
 lsr4a               macro
