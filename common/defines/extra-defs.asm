d84a0698aa2e9e8cb3730600ee1af836f7f01c0f
Doug Masten <doug@dougmasten.com>
2019-06-14 17:57:50 -0500

    add macros to extra-defs file

--- common/defines/extra-defs.asm
+++ common/defines/extra-defs.asm
@@ -13,6 +13,23 @@ ROM_MODE_ENABLE         equ $ffde           ; Any write selects RAM/ROM mode
 RAM_MODE_ENABLE         equ $ffdf           ; Any write selects all-RAM mode
 
 
+enable_normal_speed     macro
+                        sta NORMAL_SPEED_MODE
+                        endm
+
+enable_high_speed       macro
+                        sta HIGH_SPEED_MODE
+                        endm
+
+enable_rom_mode         macro
+                        sta ROM_MODE_ENABLE
+                        endm
+
+enable_ram_mode         macro
+                        sta RAM_MODE_ENABLE
+                        endm
+
+
 EXTRA_DEFS              equ 1               ; Set flag for defines being loaded
                         endc
                         *pragmapop list     ; restore assembly listing to previous state
