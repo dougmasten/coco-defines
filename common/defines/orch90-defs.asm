d90d3448cbe6e2ea4b9abf438d371f35c4be36d9
Doug Masten <doug@dougmasten.com>
2019-06-14 18:13:49 -0500

    updated orchestra-90 defines file

--- common/defines/orch90-defs.asm
+++ common/defines/orch90-defs.asm
@@ -1,13 +1,13 @@
-; Orchestra-90 Cartridge
+; orch90-defs.asm
 
                         *pragmapush list    ; Save state of list pragma
                         pragma nolist       ; Turn off assembly listing and exclude from symbol list
                         ifndef ORCH90_DEFS  ; Load defines only once
 
-
+; Orchestra-90 Cartridge
 ORCH90                  equ $ff7a           ; Orchestra-90 Cartridge
-ORCH90_LEFT             equ ORCH90          ; Orchestra-90 (Left channel)
-ORCH90_RIGHT            equ ORCH90+1        ; Orchestra-90 (Right channel)
+ORCH90_LEFT             equ ORCH90          ; Left channel
+ORCH90_RIGHT            equ ORCH90+1        ; Right channel
 
 
 ORCH90_DEFS             equ 1               ; Set flag for defines being loaded
