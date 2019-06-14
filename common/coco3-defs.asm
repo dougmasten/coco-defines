a1e4c855d27eb106cb3a564b2b261d55c50fd7fa
Doug Masten <doug@dougmasten.com>
2019-06-14 17:57:13 -0500

    move coco3-defs.asm to defines folder

--- common/coco3-defs.asm
+++ common/coco3-defs.asm
@@ -1,21 +1,2 @@
-; coco3-defs.asm
-; Tandy Color Computer 3 hardware definitions
+                    include defines/coco3-defs.asm
 
-
-                        *pragmapush list    ; Save state of list pragma
-                        pragma nolist       ; Turn off assembly listing and exclude from symbol list
-
-                        ifndef COCO3_DEFS   ; Load defines only once
-
-
-                        include defines/rsdos-defs.asm
-                        include defines/pia-defs.asm
-                        include defines/gime-defs.asm
-                        include defines/interrupts-defs.asm
-                        include defines/extra-defs.asm
-
-
-COCO3_DEFS              equ 1               ; Set flag for defines being loaded
-                        endc
-
-                        *pragmapop list     ; restore assembly listing to previous state
---
+++ common/defines/coco3-defs.asm
@@ -0,0 +1,21 @@
+; coco3-defs.asm
+; Tandy Color Computer 3 hardware definitions
+
+
+                        *pragmapush list    ; Save state of list pragma
+                        pragma nolist       ; Turn off assembly listing and exclude from symbol list
+
+                        ifndef COCO3_DEFS   ; Load defines only once
+
+
+                        include rsdos-defs.asm
+                        include pia-defs.asm
+                        include gime-defs.asm
+                        include interrupts-defs.asm
+                        include extra-defs.asm
+
+
+COCO3_DEFS              equ 1               ; Set flag for defines being loaded
+                        endc
+
+                        *pragmapop list     ; restore assembly listing to previous state
