a2fab8257c566fbe4fb0820fefa57ae409063fa7
Doug Masten <doug@dougmasten.com>
2019-09-30 12:53:53 -0500

    add comment macro

---
+++ common/macros/comments.asm
@@ -0,0 +1,18 @@
+; comments.asm
+                    *pragmapush list        ; Save state of list pragma
+                    pragma nolist           ; Turn off assembly listing and exclude from symbol list
+                    ifndef MACROS_COMMENTS_DEFS  ; Load macros only once
+
+
+comment             macro
+                    ifeq 1
+                    endm
+
+end_comment         macro
+                    endc
+                    endm
+
+MACROS_COMMENTS_DEFS equ 1                  ; Set flag for macros being loaded
+                    endc
+
+                    *pragmapop list         ; restore assembly listing to previous state
