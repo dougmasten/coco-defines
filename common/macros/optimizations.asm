abcf4b1171a37b44e92bdac4977899b22b542b1a
Doug Masten <doug@dougmasten.com>
2019-10-04 17:33:17 -0500

    add faster push and pop for single register macros to optimizations.asm

--- common/macros/optimizations.asm
+++ common/macros/optimizations.asm
@@ -72,6 +72,90 @@ long_bvs            macro
 a@                  equ *
                     endm
 
+
+; faster push and pop of a single register using SMC (Self modifying code)
+store_a             macro
+                    sta \1
+                    endm
+
+load_a              macro
+                    lda #$ff
+\1                  equ *-1
+                    endm
+
+store_b             macro
+                    stb \1
+                    endm
+
+load_b              macro
+                    ldb #$ff
+\1                  equ *-1
+                    endm
+
+store_d             macro
+                    std \1
+                    endm
+
+load_d              macro
+                    ldd #$ffff
+\1                  equ *-2
+                    endm
+
+store_e             macro
+                    ste \1
+                    endm
+
+load_e              macro
+                    lde #$ff
+\1                  equ *-1
+                    endm
+
+store_f             macro
+                    stf \1
+                    endm
+
+load_f              macro
+                    ldf #$ff
+\1                  equ *-1
+                    endm
+
+store_u             macro
+                    stu \1
+                    endm
+
+load_u              macro
+                    ldu #$ffff
+\1                  equ *-2
+                    endm
+
+store_w             macro
+                    stw \1
+                    endm
+
+load_w              macro
+                    ldw #$ffff
+\1                  equ *-2
+                    endm
+
+store_x             macro
+                    stx \1
+                    endm
+
+load_x              macro
+                    ldx #$ffff
+\1                  equ *-2
+                    endm
+
+store_y             macro
+                    sty \1
+                    endm
+
+load_y              macro
+                    ldy #$ffff
+\1                  equ *-2
+                    endm
+
+
 MACROS_OPTIMIZE_DEFS equ 1                  ; Set flag for macros being loaded
                     endc
 
