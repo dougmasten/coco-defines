7a294c2e053c0f242e759e1f4427b5ad0dd1122b
Doug Masten <doug@dougmasten.com>
2020-02-07 18:18:40 -0600

    remove tests

--- tests/test-coco3-defs.asm
+++
@@ -1,9 +0,0 @@
-; test-coco3-defs.asm
-
- include ../common/defines/coco3-defs.asm
-
-; test #1
- sta GIME_IRQ
-
-; test #2
- lda JUMP_INT_FLAG
--- tests/test-regcc-macro.asm
+++
@@ -1,9 +0,0 @@
-; test-regcc-macro.asm
-
-                include ../common/macros/regcc.asm
-
-
-
-                disable_interrupts
-                enable_interrupts
-                set_carry_flag
