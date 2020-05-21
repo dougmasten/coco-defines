dc7eb106f6056925148c0e4bbc167e8f2bc21907
Doug Masten <doug@dougmasten.com>
2020-05-20 20:18:55 -0500

    add GIMEX_DMA_DATA

--- defines/gimex-defs.asm
+++ defines/gimex-defs.asm
@@ -26,6 +26,7 @@ GIMEX_DMA_SRC_LSB       equ $ffe6           ; Source Address: bits 7-0
 GIMEX_DMA_INC           equ $ffe7           ; Auto-increment reg (1 = auto inc)
                                             ;   bit 1 = reads
                                             ;   bit 0 = writes
+GIMEX_DMA_DATA          equ $ffe8           ; 8-bit data
 GIMEX_DMA_DATA_MSB      equ $ffe8           ; 8-bit data or high byte of 16-bit data
 GIMEX_DMA_DATA_LSB      equ $ffe9           ; low byte of 16-bit data
 
