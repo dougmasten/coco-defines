6f8df4d24bfc6efbb2d39a2355ebcc70a63ab7d1
Doug Masten <doug@dougmasten.com>
2023-07-01 09:07:44 -0500

    clean up rsdos-defs.asm

--- defines/rsdos-defs.asm
+++ defines/rsdos-defs.asm
@@ -55,7 +55,6 @@ RSDOS_RVEC22            equ $1a0            ; BASIC's RAM Vector - EXBAS' Get/Pu
 RSDOS_RVEC23            equ $1a3            ; BASIC's RAM Vector - Crunch BASIC line
 RSDOS_RVEC24            equ $1a6            ; BASIC's RAM Vector - Uncrunch BASIC line
 RSDOS_VIDRAM            equ $400            ; Video Text Low-res display area
-RSDOS_VIDRAM_LEN        equ 512             ; Video Text screen size
 RSDOS_POLCAT            equ $a000           ; get a keystroke
 RSDOS_CHROUT            equ $a002           ; output a character
 RSDOS_CSRDON            equ $a004           ; turn on cassette motor, start reading
@@ -63,35 +62,9 @@ RSDOS_BLKIN             equ $a006           ; read a block from cassette
 RSDOS_BLKOUT            equ $a008           ; write a block to cassette
 RSDOS_JOYIN             equ $a00a           ; read joysticks
 RSDOS_WRTLDR            equ $a00c           ; turn on motor and write $55's to cassette
-
-
-; Disk
 RSDOS_DOS_MAGIC         equ $c000           ; flag to identify Disk ROM is present. ASCII 'DK'
 RSDOS_DSKCON            equ $c004           ; DSKCON pointer
 RSDOS_DSKVAR            equ $c006           ; address of DSKCON variables
-RSDOS_SECTOR_LEN        equ 256             ; length of sector in bytes
-RSDOS_SECTOR_MAX        equ 18              ; maximum number of sectors per track
-RSDOS_TRACK_LEN         equ RSDOS_SECTOR_LEN*RSDOS_SECTOR_MAX ; length of track in bytes
-
-RSDOS_DIR_ENTRY         struct
-DIR_NAME                rmb 8               ; filename
-DIR_EXT                 rmb 3               ; extension
-DIR_TYPE                rmb 1               ; file type
-DIR_ASCII               rmb 1               ; ASCII flag
-DIR_FIRST_GRN           rmb 1               ; First granule in file
-DIR_LAST                rmb 1               ; Number of bytes used in last sector
-                        rmb 16              ; unused
-                        ends
-
-; DSKCON Variables
-RSDOS_DSKCON_VAR        struct
-OPERATION               rmb 1               ; Operation code 0-3
-DRIVE                   rmb 1               ; Drive # 0-3
-TRACK                   rmb 1               ; Track # 0-34
-SECTOR                  rmb 1               ; Sector # 1-18
-DATA_PTR                rmb 2               ; Data Pointer
-STATUS                  rmb 1               ; Status
-                        ends
 
 
 RSDOS_DEFS              equ 1               ; Set flag for defines being loaded
